cd97bcc11a289234ca4460508907975446297f4d
Frdrcpeter007 <Frdrcpeter@gmail.com>
2020-01-02 20:11:37 +0100

    Modification du lien de connexion

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var db = require("./models/db");
 
 //mongodb+srv://frdrcpeter:mongodbpasswordmerdique@frdrcpetercluster-hiqa9.mongodb.net/test?retryWrites=true&w=majority
 //mongodb://localhost/Beniragi
-var string_con = 'mongodb://localhost/Beniragi';
+var string_con = 'mongodb+srv://frdrcpeter:mongodbpasswordmerdique@frdrcpetercluster-hiqa9.mongodb.net/test?retryWrites=true&w=majority';
 
 db.connect(string_con, (isConnected, resultConnect) => {
 
