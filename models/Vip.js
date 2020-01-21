8e624548d82cbd7a39a0da06bb31e3d92457b04b
Frdrcpeter007 <Frdrcpeter@gmail.com>
2020-01-21 10:33:21 +0100

    Affichage des VIP-Freelancers

--- models/Vip.js
+++ models/Vip.js
@@ -35,3 +35,64 @@ module.exports.becomeVIP = (newVIP, callback) => {
         callback(false, "Une exception a été lévée lors de la passation de la demande de devenir un VIP : " + exception)
     }
 }
+
+//Récupération de la liste des VIP valable à afficher 
+module.exports.getVIP = (limit, callback) => {
+    try {
+        var formatLimit = limit && parseInt(limit) ? { "$limit": parseInt(limit) } : { "$match": {} };
+
+        collection.value.aggregate([
+            {
+                "$match": {
+                    "dates.end" : { "$gte" : new Date().getTime() },
+                    "flag": true,
+                    "accept": { "$exists": 1},
+                    "accept.response": true
+                }
+            },
+            {
+                "$sort": { "created_at": 1 }
+            },
+            formatLimit
+        ]).toArray((err, resultAggr) => {
+            if (err) {
+                callback(false, "Une erreur est survenue lors de la recherche de la liste des VIP : " +err)
+            } else {
+                if (resultAggr.length > 0) {
+                    var users = require("./Users"),
+                        outVip = 0,
+                        listOut = [];
+
+                    users.initialize(db);
+                    for (let index = 0; index < resultAggr.length; index++) {
+                        var format = {
+                            "_id": resultAggr[index].id_freelancer
+                        };
+
+                        users.getInfosForFreelancer(format, (isGet, message, resultWithData) => {
+                            outVip++;
+                            if (isGet) {
+                                resultWithData.feedBacks ? delete resultWithData.feedBacks : null;
+                                resultWithData.expires = new Date(resultAggr[index].dates.end);
+
+                                listOut.push(resultWithData);
+                            }
+
+                            if (outVip == resultAggr.length) {
+                                if (listOut.length > 0) {
+                                    callback(true, "Voici la liste des VIP-BENIRAGI", listOut)
+                                } else {
+                                    callback(false, "Selon les critères d'affichage d'un freelancer nous n'avons trouvé aucun qui ait donné la permission d'affichage")
+                                }
+                            }
+                        })
+                    }
+                } else {
+                    callback(false, "Aucun VIP")
+                }
+            }
+        })
+    } catch (exception) {
+        callback(false, "Une exception a été lévée lors de la recherche de la liste des VIP : " + exception)
+    }
+}
--- routes/Vip.js
+++ routes/Vip.js
@@ -19,4 +19,19 @@ router.post('/become', (req, res) => {
     })
 })
 
+//Route pour afficher les VIP de la plateforme
+router.get('/:limit', (req, res) => {
+    var objetRetour = require("./ObjetRetour").ObjetRetour(),
+        limit = req.params.limit ? parseInt(req.params.limit) : null;
+
+    model.initialize(db);
+    model.getVIP(limit, (isGet, message, result) => {
+        objetRetour.getEtat = isGet;
+        objetRetour.getMessage = message;
+        objetRetour.getObjet = result;
+
+        res.status(200).send(objetRetour);
+    })
+})
+
 module.exports = router;
