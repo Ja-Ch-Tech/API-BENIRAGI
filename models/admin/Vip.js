6088552c3d7743d640f0462571acf46a42b8af76
Frdrcpeter007 <frdrcpeter@gmail.com>
2020-03-01 01:31:37 +0100

    Fixation des images sur la liste des VIP dans l'admin

--- models/admin/Vip.js
+++ models/admin/Vip.js
@@ -30,11 +30,11 @@ module.exports = {
                     findOneByIdAndNotTreat(objet.id_vip, (isFound, message, resultFound) => {
                         if (isFound) {
                             var filter = {
-                                    "_id": resultFound._id  
-                                },
+                                "_id": resultFound._id
+                            },
                                 update = {
                                     "$set": {
-                                        "accept": { response: objet.response == "true" ? true : false, id_admin: objet.id_admin},
+                                        "accept": { response: objet.response == "true" ? true : false, id_admin: objet.id_admin },
                                         "dates.end": new Date().getTime() + parseInt(resultFound.dates.duration) * 30 * 24 * 60 * 60 * 1000,
                                         "flag": false
                                     }
@@ -42,12 +42,12 @@ module.exports = {
 
                             collection.value.updateOne(filter, update, (err, resultUp) => {
                                 if (err) {
-                                    callback(false, "Une erreur lors de la mise à jour de la reponse accordé à la demande : " +err)
+                                    callback(false, "Une erreur lors de la mise à jour de la reponse accordé à la demande : " + err)
                                 } else {
                                     if (resultUp) {
                                         callback(true, "La reponse de l'administration a été renvoyé", resultUp)
                                     } else {
-                                        callback(false, "Aucune mise à jour")                
+                                        callback(false, "Aucune mise à jour")
                                     }
                                 }
                             })
@@ -79,11 +79,11 @@ module.exports = {
                             "$match": {
                                 "flag": true,
                                 "$or": [
-                                    {"accept": {"$exists": 0}},
+                                    { "accept": { "$exists": 0 } },
                                     {
                                         "$and": [
-                                            {"accept": {"$exists": 1}},
-                                            {"accept.response": false}
+                                            { "accept": { "$exists": 1 } },
+                                            { "accept.response": false }
                                         ]
                                     }
                                 ]
@@ -91,7 +91,7 @@ module.exports = {
                         }
                     ]).toArray((err, resultAggr) => {
                         if (err) {
-                            callback(false, "Une erreur est survenue lors de la récupération des nouvelles demande : " +err)
+                            callback(false, "Une erreur est survenue lors de la récupération des nouvelles demande : " + err)
                         } else {
                             if (resultAggr.length > 0) {
                                 var outVip = 0,
@@ -107,8 +107,8 @@ module.exports = {
                                         outVip++;
                                         if (isGet) {
                                             listOut.push({
-                                                infos: resultWithData, 
-                                                id_vip: resultAggr[index]._id, 
+                                                infos: resultWithData,
+                                                id_vip: resultAggr[index]._id,
                                                 created_at: resultAggr[index].created_at
                                             })
                                         }
@@ -132,7 +132,7 @@ module.exports = {
                 }
             })
         } catch (exception) {
-            
+
         }
     },
 
@@ -142,7 +142,7 @@ module.exports = {
      * @param {Function} callback 
      */
     getAnyVIPRequest(objet, callback) {
-        try{
+        try {
             admin.initialize(db);
             admin.isAdmin(objet.id_admin, (isTrue, message, result) => {
                 if (isTrue) {
@@ -150,10 +150,10 @@ module.exports = {
                         {
                             "$match": {}
                         },
-                        { "$sort": {"created_at": -1}}
+                        { "$sort": { "created_at": -1 } }
                     ]).toArray((err, resultAggr) => {
                         if (err) {
-                            callback(false, "Une erreur est survenue lors de la récupération des demandes VIP : " +err)
+                            callback(false, "Une erreur est survenue lors de la récupération des demandes VIP : " + err)
                         } else {
                             if (resultAggr.length > 0) {
                                 var outVip = 0,
@@ -171,20 +171,21 @@ module.exports = {
                                         if (isGet) {
                                             var infosUser = {
                                                 "email": resultWithData.email,
-                                                "identity": resultWithData.identity ? resultWithData.identity : null
+                                                "identity": resultWithData.identity ? resultWithData.identity : null,
+                                                "avatar": resultWithData.avatar ? resultWithData.avatar : null
                                             },
-                                            out = {
-                                                infosUser,
-                                                infosVIP: {
-                                                    id_vip: resultAggr[index]._id,
-                                                    dates: {
-                                                        begin: resultAggr[index].accept ? getBeginDate(resultAggr[index].dates) : null,
-                                                        end: resultAggr[index].accept ? new Date(resultAggr[index].dates.end).toISOString() : null
-                                                    },
-                                                    action: resultAggr[index].accept ? (resultAggr[index].accept.response == true ? (resultAggr[index].dates.end >= new Date().getTime() ? "stop" : "Renouveler") : "Renouveler"): "Donner accord",
-                                                    status: resultWithData.flag == true ? resultWithData.certicate && resultWithData.certicate.certified ? "Certifié" : null: "Arrêter"
+                                                out = {
+                                                    infosUser,
+                                                    infosVIP: {
+                                                        id_vip: resultAggr[index]._id,
+                                                        dates: {
+                                                            begin: resultAggr[index].accept ? getBeginDate(resultAggr[index].dates) : null,
+                                                            end: resultAggr[index].accept ? new Date(resultAggr[index].dates.end).toISOString() : null
+                                                        },
+                                                        action: resultAggr[index].accept ? (resultAggr[index].accept.response == true ? (resultAggr[index].dates.end >= new Date().getTime() ? "stop" : "Renouveler") : "Renouveler") : "Donner accord",
+                                                        status: resultWithData.flag == true ? resultWithData.certicate && resultWithData.certicate.certified ? "Certifié" : null : "Arrêter"
+                                                    }
                                                 }
-                                            }
 
                                             listOut.push(out)
                                         }
@@ -207,7 +208,7 @@ module.exports = {
                     callback(false, message)
                 }
             })
-        }catch(exception) {
+        } catch (exception) {
 
         }
     },
@@ -226,17 +227,17 @@ module.exports = {
                         {
                             "$match": {
                                 "_id": require("mongodb").ObjectId(objet.id_vip),
-                                "accept": {"$exists": 1}
+                                "accept": { "$exists": 1 }
                             }
                         }
                     ]).toArray((err, resultAggr) => {
                         if (err) {
-                            callback(false, "Une erreur lors de la recherche du vip : " +err)
+                            callback(false, "Une erreur lors de la recherche du vip : " + err)
                         } else {
                             if (resultAggr.length > 0) {
                                 var filter = {
-                                        "_id": resultAggr[0]._id
-                                    },
+                                    "_id": resultAggr[0]._id
+                                },
                                     update = {
                                         "$set": {
                                             "accept": { response: resultAggr[0].accept.response == true ? false : true, id_admin: objet.id_admin },
@@ -256,7 +257,7 @@ module.exports = {
                             }
                         }
                     })
-                   
+
                 } else {
                     callback(false, message)
                 }
@@ -290,7 +291,7 @@ const findOneByIdAndNotTreat = (id, callback) => {
         }
     ]).toArray((err, resultAggr) => {
         if (err) {
-            callback(false, "Une erreur est survenue lors de la recherche d'une demande VIP : " +err)
+            callback(false, "Une erreur est survenue lors de la recherche d'une demande VIP : " + err)
         } else {
             if (resultAggr.length > 0) {
                 callback(true, "La demande à été trouvé", resultAggr[0])
